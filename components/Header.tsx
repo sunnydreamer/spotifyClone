c1a660008cfbbfb09d406f4f18d31adc225672aa
sunnydreamer <sunnylee9516@gmail.com>
2023-07-23 23:01:27 -0400

    completed layout setting

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,10 @@
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
-import { HiHome, HiSearch } from "react-icons/hi";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+
+import Button from "./Button";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -83,10 +86,46 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             transition
             "
           >
-            <HiSearch className="text-black" size={20} />
+            <BiSearch className="text-black" size={20} />
           </button>
         </div>
+        <div
+          className="
+            flex
+            justify-between
+            items-center
+            gap-x-4
+        "
+        >
+          <>
+            <div>
+              <Button
+                onClick={() => {}}
+                className="
+              bg-transparent
+              text-neutral-300
+              font-medium
+              "
+              >
+                Sign Up
+              </Button>
+            </div>
+            <div>
+              <Button
+                onClick={() => {}}
+                className="
+              bg-white
+              px-6
+              py-2
+              "
+              >
+                Log in
+              </Button>
+            </div>
+          </>
+        </div>
       </div>
+      {children}
     </div>
   );
 };
