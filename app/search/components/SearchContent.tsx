71fe0e1851d9d53f583ed8be7f9570ac8515a494
sunnydreamer <sunnylee9516@gmail.com>
2023-08-25 23:41:09 -0400

    like songs

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import MediaItem from "@/components/MediaItem";
+import LikeButton from "@/components/LikeButton";
 import { Song } from "@/types";
 
 interface SearchContentProps {
@@ -32,7 +33,7 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
           <div className="flex-1">
             <MediaItem onClick={() => {}} data={song} />
           </div>
-          {/* TODO: add like button here */}
+          <LikeButton songId={song.id} />
         </div>
       ))}
     </div>
